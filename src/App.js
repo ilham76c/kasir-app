22512d1f1071f8218e5c2f02bfb0f4955ade9c50
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-14 02:13:07 +0700

    setting layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import NavbarComponent from './components/NavbarComponent';
+import { Row, Col, Container } from 'react-bootstrap'
+import { Hasil, ListCategories, NavbarComponent } from './components';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4>
+                <strong>Daftar Product</strong>
+              </h4>
+              <hr/>
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
