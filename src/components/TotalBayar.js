d444ffdf37b672157254bcbf06afe67212cf1049
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-18 01:58:53 +0700

    menambahkan tombol BAYAR

--- src/components/TotalBayar.js
+++ src/components/TotalBayar.js
@@ -1,5 +1,7 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { numberWithCommas } from '../utils/utils';
 
 export default class TotalBayar extends Component {
@@ -12,7 +14,17 @@ export default class TotalBayar extends Component {
             <div className="fixed-bottom">
                 <Row>
                     <Col md={{ span: 3, offset: 9 }} className="px-4">
-                        <h4>Total Harga : <strong className="float-right">Rp. { numberWithCommas(totalBayar) }</strong></h4>
+                        <h4>
+                            Total Harga :{" "}
+                            <strong className="float-right">
+                                Rp. { numberWithCommas(totalBayar) }
+                            </strong>
+                        </h4>
+                        <div className="d-grid gap-2">
+                            <Button variant="primary" className="my-2" size="lg">
+                                <FontAwesomeIcon icon={faShoppingCart} /> <strong>BAYAR</strong>
+                            </Button>
+                        </div>
                     </Col>
                 </Row>
             </div>
