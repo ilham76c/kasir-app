3f4a8455e6fda1969b1389bb637420189b7533c4
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-10-18 02:19:32 +0700

    menghapus data keranjang setelah proses bayar atau pesanan sukses

--- src/pages/Sukses.js
+++ src/pages/Sukses.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
     Link,
 } from "react-router-dom";
-import { Button, Image } from 'react-bootstrap'
+import { Button, Image } from 'react-bootstrap';
+import axios from 'axios';
+import { API_URL } from '../utils/constant';
 
 export default class Sukses extends Component {
+
+    componentDidMount() {
+        axios.get(`${API_URL}/keranjangs`)
+            .then(res => {
+                const keranjangs = res.data;
+                keranjangs.map((item) => {
+                    return axios.delete(`${API_URL}/keranjangs/${item.id}`)
+                                .then((res) => {
+                                    console.log(res);
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     render() {
         return (
             <div className="mt-4 text-center">
